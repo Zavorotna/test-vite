26be63e5d5e2be535bab4e246f967a0c224b84fd
Zavorotna <zavorotnaa96@gmail.com>
2025-08-21 13:35:07 +0300

    test project vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/repo-name/', // <-- назва твого репозиторію на GitHub
+  base: '/test-vite/', // <-- назва твого репозиторію на GitHub
   build: {
     outDir: 'docs', // куди збирати
   },
